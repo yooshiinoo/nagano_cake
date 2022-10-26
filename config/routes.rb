6d7861339e6d75b8d2862a5c69471efab9fa5529
EC2 Default User <ec2-user@ip-172-31-43-45.ap-northeast-1.compute.internal>
2022-10-26 05:18:42 +0000

    stop5

--- config/routes.rb
+++ config/routes.rb
@@ -17,19 +17,20 @@ Rails.application.routes.draw do
   namespace :public do
     root to: "homes#top"
     get "/about" =>"homes#about"
-    resources :items
-    resources :customers
-    resources :cart_items
-    resources :orders
-    resources :addresses
+    resources :items, only: [:index, :show]
+    resources :customers, only: [:show, :edit, :update, :unsubscribe, :withdraw]
+    resources :cart_items, only: [:index, :update, :destroy, :destroy_all, :create, ]
+    resources :orders, only: [:new, :comfirm, :complete, :create, :index, :show,]
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
   end
 
   namespace :admin do
     root "homes#top"
-    resources :items
-    resources :genres
-    resources :customers
-    resources :orders
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+    resources :order_details, only: [:update]
   end
 
 
