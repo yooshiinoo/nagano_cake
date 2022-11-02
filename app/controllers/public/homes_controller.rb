1bd6349fd64fbac9142a4e92c831a05c1d03769d
EC2 Default User <ec2-user@ip-172-31-43-45.ap-northeast-1.compute.internal>
2022-11-02 11:20:21 +0000

    stop8

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,10 @@
 class Public::HomesController < ApplicationController
   def top
+    @guides = Genre.all
+    @items = Item.all
   end
 
   def about
+    @item = Item.all
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,8 @@ class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :order_details, dependent: :destroy
   belongs_to :genre
+
+  def with_tax_price
+    (price * 1.1).floor
+  end
 end
