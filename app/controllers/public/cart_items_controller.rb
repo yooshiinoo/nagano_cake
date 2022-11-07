c96f050cdf6c98379c13aecb2253e0148cd6de74
EC2 Default User <ec2-user@ip-172-31-43-45.ap-northeast-1.compute.internal>
2022-11-07 07:09:50 +0000

    stop11

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,23 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @cart_item = Cart_item.new
+    
   end
+  
+  def create
+    @cart_item = Cart_item.new(cart_item_params)
+    if @cart_item.save
+      redirect_to public_cart_items_path(@cart_item)
+    else
+      @cart_item = Cart_item.new
+      render :index
+    end
+  end
+  
+  private
+  def cart_item_params
+      params.require(:cart_item).permit(:item_id, :customer_id, :amount)
+  end
+  
 end
+cart_item_params[:item_id]
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -8,5 +8,23 @@ class Public::ItemsController < ApplicationController
   def show
     @guides = Genre.all
     @item = Item.find(params[:id])
+    @cart_item = CartItem.new
   end
+
+  def create
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+    @cart_item.save
+    redirect_to public_item_path(@item)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :genre_id, :price, :is_active)
+  end
+
+  def cart_item_params
+      params.require(:cart_item).permit(:item_id, :amount)
+  end
+
 end
