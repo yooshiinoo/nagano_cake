416be9444b9982018f917d5833206102181c058c
EC2 Default User <ec2-user@ip-172-31-43-45.ap-northeast-1.compute.internal>
2022-11-08 06:13:10 +0000

    stop12

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,23 +1,29 @@
 class Public::CartItemsController < ApplicationController
   def index
-    @cart_item = Cart_item.new
-    
+    @cart_items = current_customer.cart_items
+    @cart_item = cart_item_params[:item_id]
   end
-  
+
   def create
-    @cart_item = Cart_item.new(cart_item_params)
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
     if @cart_item.save
-      redirect_to public_cart_items_path(@cart_item)
+      redirect_to public_cart_item_path(current_customer)
     else
-      @cart_item = Cart_item.new
+      @cart_item = CartItem.new
       render :index
     end
   end
-  
+
+  def update
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to public_cart_items_path(current_customer)
+  end
+
   private
   def cart_item_params
       params.require(:cart_item).permit(:item_id, :customer_id, :amount)
   end
-  
+
 end
-cart_item_params[:item_id]
