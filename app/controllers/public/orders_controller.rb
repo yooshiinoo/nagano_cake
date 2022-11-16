afb5dbbd310defb0303566f32e73c79da949f1a1
EC2 Default User <ec2-user@ip-172-31-43-45.ap-northeast-1.compute.internal>
2022-11-16 16:15:12 +0000

    stop15

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -12,12 +12,56 @@ class Public::OrdersController < ApplicationController
     @order = Order.new(
       customer: current_customer,
       payment_method: params[:order][:payment_method])
-
+      
+    if params[:order][:select_address] == "1"
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = full_name(current_customer)
+      
+    elsif params[:order][:select_address] == "2"
+      ship = Address.find(params[:order][:address_id])
+      @order.postal_code = ship.postal_code
+      @order.address = ship.address
+      @order.name = ship.name
+      
+    elsif params[:order][:select_address] == "3"
+      @order.postal_code = params[:order][:postal_code]
+      @order.address = params[:order][:address]
+      @order.name = params[:order][:name]
+      @ship = "1"
+      
+      unless @order.valid? == true
+        @addresses = Address.where(customer: current_customer)
+        render :new
+      end
+    end
   end
 
   def complete
   end
 
+  def create
+    @order = current_customer.order.new(order_params)
+    @order.save
+    
+    if params[:order][:ship] == "1"
+      current_customer.address.create(address_params)
+    end
+    
+    @cart_item = current_customer.cart_item
+    @cart_item.each do |cart_item|
+      @order_detail = OrderDetail.new
+      @order_detail.item_id = cart_item.item_id
+      @order_detail.order_id = @order.id
+      @order_detail.amount = cart_item.amount
+      @order_detail.price = @order.with_tax_price
+      @order_detail.save
+    end
+    
+    @cart_items.destroy_all
+    redirect_to public_orders_complete_path
+  end
+  
   def index
     @orders = current_customer.orders
   end
@@ -26,11 +70,7 @@ class Public::OrdersController < ApplicationController
     @order = Order.find(params[:id])
     @order_details = @order.order_details
   end
-
-  def create
-  end
-
-
+  
 
   def total_payment
     @total + @order.shipping_cost
@@ -39,6 +79,10 @@ class Public::OrdersController < ApplicationController
   private
   def order_params
     params.require(:order).permit(:customer_id, :postal_code, :address, :name, :shipping_cost, :total_payment, :payment_method, :status)
+  end
+  
+  def address_params
+    params.require(:order).permit(:postal_code, :address, :name)
   end
 
 end
